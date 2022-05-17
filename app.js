72d23e6c1ad39c1be1ae152ff38eeeef409cd594
Ioana Seres <ioana.cosoreanuseres@888holdings.com>
2022-05-17 16:48:23 +0300

    final commit

--- app.js
+++ app.js
@@ -23,21 +23,35 @@ const ctaBtn = document.querySelector(".CTA");
 
 ctaBtn.addEventListener("click", function () {
   ctaBtn.textContent = "Join our team today!";
-  ctaBtn.style.background = " rgba(198, 210, 246, 1)";
+  localStorage.setItem("ctaBtnText", "Join our team today!");
+  ctaBtn.style.background = "rgba(198, 210, 246, 1)";
+  localStorage.setItem("background", "rgba(198, 210, 246, 1)");
 });
 
-localStorage.setItem("ctaBtnText", "Join our team today!");
-localStorage.setItem("background", "rgba(198, 210, 246, 1)");
+window.addEventListener("load", function () {
+  let ctaBtnText = localStorage.getItem("ctaBtnText");
+  let background = localStorage.getItem("background");
 
-// window.addEventListener("popstate", function () {
-//   ctaBtn.textContent = localStorage.getItem("ctaBtnText");
-//   ctaBtn.style.background = localStorage.getItem("background");
-// });
+  if (ctaBtnText) {
+    ctaBtn.textContent = ctaBtnText;
+  } else {
+    ctaBtn.textContent = "Got what it takes?";
+  }
+  if (background) {
+    ctaBtn.style.background = background;
+  } else {
+    ctaBtn.style.background =
+      "linear-gradient( 90deg, rgba(222, 162, 31, 1) 28%,rgba(247, 190, 109, 1) 60%);";
+  }
+});
 
-//alert
-// setTimeout(() => {
-//   alert("No longer available");
-//   if (alert) {
-//     window.location.href = "status_expired.html";
-//   }
-// }, 3000);
+alert;
+setTimeout(() => {
+  alert("No longer available");
+  if (alert) {
+    window.location.href = "job_status.php";
+  }
+}, 300000);
+
+// localStorage.removeItem("ctaBtnText");
+// localStorage.removeItem("background");
